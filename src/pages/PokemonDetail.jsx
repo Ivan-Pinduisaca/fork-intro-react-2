7877a99fdac6735d55d3dc71b06c4d4254e828e1
Ivan Pinduisaca <ivanpindui99@gmail.com>
2022-05-26 15:23:55 -0500

    Implementado pagina de error

--- src/pages/PokemonDetail.jsx
+++ src/pages/PokemonDetail.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { Error404 } from "./Error404";
 
 export const PokemonDetail = () => {
   const [pokemon, setPokemon] = useState(null);
@@ -28,7 +29,10 @@ export const PokemonDetail = () => {
       .then((pokemon) => {
         setPokemon(pokemon);
       })
-      .catch((error) => {});
+      .catch((error) => {
+        setError(true);
+        console.log(error);
+      });
   }, []);
 
   return (
@@ -67,8 +71,28 @@ export const PokemonDetail = () => {
           </div>
         </>
       ) : (
-        <>Loading</>
+        <>
+          <div>
+            <h1>{!error ? (
+              <div className="grid mt-5 justify-items-center">
+                <div className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">O</div>
+                Loading...
+              </div>
+            ) : (
+              <>
+                <div className='flex flex-row justify-evenly m-5 p-2'>
+                  <h1 className='text-3xl'><Error404 />El pokemon con id '{pokemonId}' no existe</h1>
+                </div>
+                <div className='flex flex-row justify-evenly m-5 p-2'>
+                  <button className='bg-slate-800 rounded-md p-3 text-white hober:bg-slate-900' onClick={goBack}>Regresar</button>
+                </div>
+              </>
+            )}
+            </h1>
+          </div>
+
+        </>
       )}
     </div>
-  );
-};
+  )
+}
